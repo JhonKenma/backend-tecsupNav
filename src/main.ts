fbc059d7fe838f00c9f343b91c1a766eb697eb15
Jhons Kenma <jhonskenma@gmail.com>
2025-10-21 03:55:50 -0500

    chore(main): actualizar configuración de servidor
    - Ajustar configuración CORS si aplica
    - Actualizar configuración de WebSocket si aplica

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
       process.env.MOBILE_URL,
     ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
