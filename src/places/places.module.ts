9b3ebd7f767dcc03f9f74cb3f09683df736cd253
Jhons Kenma <jhonskenma@gmail.com>
2025-11-07 18:18:53 -0500

    feat(places): importar StorageModule para habilitar subida de imágenes en lugares

--- src/places/places.module.ts
+++ src/places/places.module.ts
@@ -1,9 +1,10 @@
-// src/places/places.module.ts
 import { Module } from '@nestjs/common';
 import { PlacesService } from './places.service';
 import { PlacesController } from './places.controller';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
+  imports: [StorageModule], // Importar el módulo de storage
   controllers: [PlacesController],
   providers: [PlacesService],
   exports: [PlacesService],
